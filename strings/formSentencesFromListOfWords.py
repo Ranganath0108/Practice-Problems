43008ce81e1edc3ae496e54af532d6054051316d
Ranganath0108 <ranganathswamy.ys@gmail.com>
2022-04-23 15:17:58 +0530

    Form sentences Questionis added1

--- strings/formSentencesFromListOfWords.py
+++ strings/formSentencesFromListOfWords.py
@@ -1,17 +1,17 @@
-def formSentences(words,result='',n=0):
+def formSentences(words, result='', n=0):
 
     if not words:
         return
 
-    if(n==len(words)):
+    if(n == len(words)):
         print(result)
         return
 
     for word in words[n]:
-        out=result+" "+word
-        formSentences(words,out,n+1)
+        out = result+" "+word
+        formSentences(words, out, n+1)
 
 
 print(formSentences([["you", "we"],
-                ["have", "are"],
-                ["sleep", "eat","drink"]]))
+                     ["have", "are"],
+                     ["sleep", "eat", "drink"]]))
