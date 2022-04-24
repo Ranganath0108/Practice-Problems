d735908f6fa8689c75459d49b949e73e1fb3a7d0
Ranganath0108 <ranganathswamy.ys@gmail.com>
2022-04-24 06:57:02 +0530

    added contents

--- BinaryTree/VerticalOrder.py
+++ BinaryTree/VerticalOrder.py
@@ -3,25 +3,29 @@ from parase import parse_tuple
 
 
 def VerticalOrder_traversal(root):
-    m=dict()
-    hd=0
-    res=[]
-    getVerticalOrder(root,hd,m)
+    m = dict()
+    hd = 0
+    res = []
+    getVerticalOrder(root, hd, m)
 
     for i in sorted(m):
-        res.append({i:m[i]})
+        res.append({i: m[i]})
     return res
-        
 
-def getVerticalOrder(root,hd,m):
+
+def getVerticalOrder(root, hd, m):
     if root is None:
-        return 
+        return
     try:
         m[hd].append(root.data)
     except:
-        m[hd]=[root.data]
-    getVerticalOrder(root.left,hd-1,m)
-    getVerticalOrder(root.right,hd+1,m)
+        m[hd] = [root.data]
+    getVerticalOrder(root.left, hd-1, m)
+    getVerticalOrder(root.right, hd+1, m)
+
+
 if __name__ == "__main__":
     root = parse_tuple(tree1)
     print(VerticalOrder_traversal(root))
+    root2=parse_tuple(tree2)
+    print(VerticalOrder_traversal(root2))
