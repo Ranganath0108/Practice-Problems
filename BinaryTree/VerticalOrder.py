a56e1321ad39e94e849bce3759191baaab32d787
Ranganath0108 <ranganathswamy.ys@gmail.com>
2022-04-24 06:31:29 +0530

    Vertical Order traversal added

---
+++ BinaryTree/VerticalOrder.py
@@ -0,0 +1,27 @@
+from ListOftrees import *
+from parase import parse_tuple
+
+
+def VerticalOrder_traversal(root):
+    m=dict()
+    hd=0
+    res=[]
+    getVerticalOrder(root,hd,m)
+
+    for i in sorted(m):
+        res.append({i:m[i]})
+    return res
+        
+
+def getVerticalOrder(root,hd,m):
+    if root is None:
+        return 
+    try:
+        m[hd].append(root.data)
+    except:
+        m[hd]=[root.data]
+    getVerticalOrder(root.left,hd-1,m)
+    getVerticalOrder(root.right,hd+1,m)
+if __name__ == "__main__":
+    root = parse_tuple(tree1)
+    print(VerticalOrder_traversal(root))
