59efc92fd3b8ab82427b88e5f10af5229d7a6b62
Ranganath0108 <ranganathswamy.ys@gmail.com>
2022-04-24 07:47:00 +0530

    few changes in isomorphic

--- strings/isomorphic.py
+++ strings/isomorphic.py
@@ -2,20 +2,21 @@
 
 
 def is_Isomorphic(s1, s2):
-    iso_map = dict()
+    map1 = dict()
+    map2=dict()
 
     if(len(s1) != len(s2)):
         return False
-    else:
-        for i, j in zip(s1, s2):
-            if i not in iso_map:
-                iso_map[i] = j
-            else:
-                if(iso_map[i] != j):
-                    return False
+    for i, j in zip(s1, s2):
+        if i not in map1 and j not in map2:
+            map1[i] = j
+            map2[j]=i
+        elif (map1.get(i)!=j or map2.get(j)!=i):
+            return False
     return True
 
 
 print(is_Isomorphic("aab", "xxy"))
 print(is_Isomorphic("foo", "bar"))
 print(is_Isomorphic("paper", 'title'))
+print(is_Isomorphic("pijthbsfy","fvladzpbf"))
